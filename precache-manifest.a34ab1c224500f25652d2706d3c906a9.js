06f8824d11446dc744fe1e8c00b79210f377d58d
Nitesh Virani <niteshvirani@gmail.com>
2019-01-12 10:24:03 +0530

    Updates

---
+++ precache-manifest.a34ab1c224500f25652d2706d3c906a9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "11c07349d0ac34c40e4759de3bc90bcf",
+    "url": "/static/media/waveum.11c07349.png"
+  },
+  {
+    "revision": "1ee99508aa57adf19d81a182b1ba84bb",
+    "url": "/static/media/sopotra.1ee99508.png"
+  },
+  {
+    "revision": "7f7314743c2ec39faa62d0c3c52b506b",
+    "url": "/static/media/shiperd.7f731474.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "afbe6f2bceb5c925c3f4",
+    "url": "/static/js/main.afbe6f2b.chunk.js"
+  },
+  {
+    "revision": "8e5e6baafa1df7e73b25",
+    "url": "/static/js/1.8e5e6baa.chunk.js"
+  },
+  {
+    "revision": "afbe6f2bceb5c925c3f4",
+    "url": "/static/css/main.dc5ddda4.chunk.css"
+  },
+  {
+    "revision": "790124120640337d546846fe2448c34e",
+    "url": "/index.html"
+  }
+];
